0729fe6bb691c816a09d1e393cebb3d19f8cd91e
BennyHunag <p1472222@gmail.com>
2022-04-01 21:42:30 +0800

    破壞衝突!

--- TuanBuy/Service/LoginAndRegisterController.cs
+++ TuanBuy/Service/LoginAndRegisterController.cs
@@ -66,46 +66,35 @@ namespace TuanBuy.Service
             if (user == null || user.Email == null || loginUser.Email == "" || loginUser.Password == "")
                 return "帳號密碼錯誤";
 
-            if (user.State == 0)
-            {
-                return "帳號未啟用";
-            }
-            else
-            {
-                var claims = new List<Claim>
-                {
-                    new Claim(ClaimTypes.Name,user.NickName),
-                    new Claim(ClaimTypes.Email,user.Email),
-                    new Claim("Userid",user.Id.ToString()),
-                    new Claim("NickName",user.NickName),
-                    new Claim("Email",user.Email.ToString()),
-                    new Claim("UserName",user.Name.ToString())
+            if (user.State == 0) return "帳號未啟用";
 
+            var claims = new List<Claim>
+            {
+                new(ClaimTypes.Name, user.NickName),
+                new(ClaimTypes.Email, user.Email),
+                new("Userid", user.Id.ToString()),
+                new("NickName", user.NickName),
+                new("Email", user.Email),
+                new("UserName", user.Name),
+                new Claim("PicPath",user.PicPath),
+            };
+            //將使用者資訊存入session
+            var jsonstring = JsonConvert.SerializeObject(new
+            {
+                user.Email,
+                user.NickName,
+                user.Id,
+                user.PicPath
+            });
+            HttpContext.Session.SetString("userData", jsonstring);
+            if (user.State >= 1) claims.Add(new Claim(ClaimTypes.Role, "User"));
+            if (user.State == 2) claims.Add(new Claim(ClaimTypes.Role, "FullUser"));
 
-                };
-                //將使用者資訊存入session
-                string jsonstring = Newtonsoft.Json.JsonConvert.SerializeObject(new
-                {
-                    Email = user.Email,
-                    NickName = user.NickName,
-                    Id = user.Id
-                });
-                HttpContext.Session.SetString("userData", jsonstring);
-                if (user.State >= 1)
-                {
-                    claims.Add(new Claim(ClaimTypes.Role, "User"));
-                }
-                if (user.State == 2)
-                {
-                    claims.Add(new Claim(ClaimTypes.Role, "FullUser"));
-                }
-                
-                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-                var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
-                HttpContext.SignInAsync(claimsPrincipal);
-                //HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
-                return "登入成功";
-            }
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+            HttpContext.SignInAsync(claimsPrincipal);
+            //HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+            return "登入成功";
         }
 
         [HttpDelete]
