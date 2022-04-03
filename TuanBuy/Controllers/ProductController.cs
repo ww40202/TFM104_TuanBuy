411ceb233e15a2bdded9aafe54c95640427b9433
BennyHunag <p1472222@gmail.com>
2022-04-04 04:07:18 +0800

    Revert "Merge branch 'devop' into Benny"
    This reverts commit ed63e7615c9a08f88de26591779c487efc8924c9, reversing
    changes made to 868a39c0c7a970ec652ae3bae7a0f489f5dc638d.

--- TuanBuy/Controllers/ProductController.cs
+++ TuanBuy/Controllers/ProductController.cs
@@ -18,13 +18,14 @@ namespace TuanBuy.Controllers
         private readonly IRepository<Product> _productsRepository;
         private readonly IWebHostEnvironment _environment;
         private readonly IRepository<User> _userRepository;
-        private readonly TuanBuyContext _dbContext;
-        public ProductController(GenericRepository<Product> productsRepository, IWebHostEnvironment environment, GenericRepository<User> userRepository, TuanBuyContext dbContext)
+        private TuanBuyContext _sqldb;
+
+        public ProductController(GenericRepository<Product> productsRepository, IWebHostEnvironment environment, GenericRepository<User> userRepository, TuanBuyContext sqldb)
         {
             _productsRepository = productsRepository;
             _environment = environment;
             _userRepository = userRepository;
-            _dbContext = dbContext;
+            _sqldb = sqldb;
         }
         //新增商品首頁
         [Authorize(Roles = "FullUser")]
@@ -32,13 +33,6 @@ namespace TuanBuy.Controllers
         {
             return View();
         }
-        //新增商品頁
-        public IActionResult Test()
-        {
-            return View();
-        }
-
-        //商品介紹頁
         public IActionResult DemoProduct()
         {
             return View();
@@ -47,7 +41,7 @@ namespace TuanBuy.Controllers
         [HttpGet]
         public IActionResult DemoProduct(int id)
         {
-            ProductManage product = new ProductManage(_dbContext);
+            ProductManage product = new ProductManage(_sqldb);
             var result = product.GetDemoProductData(id);
             return View(result);
         }
@@ -68,71 +62,50 @@ namespace TuanBuy.Controllers
         [Authorize(Roles = "FullUser")]
         public IActionResult PostProduct(AddProductViewModel product)
         {
+            var path = _environment.WebRootPath + "/ProductPicture";
             if (product.PicPath == null)
             {
                 return BadRequest();
             }
+            var pic = product.PicPath.FirstOrDefault();
 
-
-            var targetUser = GetTargetUser();
-
-            #region 建立商品
-            var targetProduct = new Product
+            if (pic != null)
             {
-                Name = product.Name,
-                Content = product.Content,
-                Category = product.Category,
-                Description = product.Description,
-                CreateTime = DateTime.Now,
-                EndTime = product.EndTime,
-                Price = product.Price,
-                User = targetUser
-            };
-            _productsRepository.Create(targetProduct);
-            _productsRepository.SaveChanges();
-            #endregion
+                var fileName = DateTime.Now.Ticks.ToString() + pic.FileName;
 
-            //檔案路徑
-            var path = _environment.WebRootPath + "/ProductPicture";
-
-            #region 加入圖片
-            
-            
-            foreach (var file in product.PicPath)
-            {
-                if (file != null)
+                using (var fs = System.IO.File.Create($"{path}/{fileName}"))
                 {
-                    var fileName = DateTime.Now.Ticks + file.FileName;
-                    using var fs = System.IO.File.Create($"{path}/{fileName}");
-                    file.CopyTo(fs);
-                    var pPic = new ProductPic()
-                    {
-                        Product = targetProduct,
-                        PicPath = fileName
-                    };
-                    _dbContext.ProductPics.Add(pPic);
+                    pic.CopyTo(fs);
                 }
+                
+                var claim = HttpContext.User.Claims;
+                var userEmail = claim.FirstOrDefault(a => a.Type == ClaimTypes.Email)?.Value;
+                var targetUser = _userRepository.Get(a => a.Email == userEmail);
+                
+                _productsRepository.Create(new Product()
+                {
+                    Name = product.Name,
+                    PicPath = fileName,
+                    Content = product.Content,
+                    Category = product.Category,
+                    Description = product.Description,
+                    CreateTime = DateTime.Now,
+                    EndTime = product.EndTime,
+                    Price = product.Price,
+                    User = targetUser
+                });
+                return Ok();
             }
-            _dbContext.SaveChanges();
-
-
-            #endregion
-            return Ok();
+            else
+            {
+                return BadRequest();
+            }
         }
-
 
         [Authorize(Roles = "FullUser")]
         public IActionResult MyProduct()
         {
             return View();
-        }
-        //抓取當前使用者
-        private User GetTargetUser()
-        {
-            var claim = HttpContext.User.Claims;
-            var userEmail = claim.FirstOrDefault(a => a.Type == ClaimTypes.Email)?.Value;
-            var targetUser = _userRepository.Get(a => a.Email == userEmail);
-            return targetUser;
         }
     }
 }
