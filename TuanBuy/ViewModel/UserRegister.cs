f20ad7c9b1887c48d73cfd3c2a98ddde02e4eece
Lynn <lynnmaxwell0706@gmail.com>
2022-04-08 21:46:57 +0800

    reset

--- TuanBuy/ViewModel/UserRegister.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TuanBuy.ViewModel
-{
-    public class UserRegister
-    {
-        public string Email { get; set; }
-        public string Name { get; set; }
-        public string Password { get; set; }
-    }
-}
