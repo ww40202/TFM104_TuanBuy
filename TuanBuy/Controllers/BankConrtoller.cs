81cc18748a763339738d37d8a552219409edcc40
BennyHunag <p1472222@gmail.com>
2022-04-13 20:24:11 +0800

    更新金流結帳訂單

--- TuanBuy/Controllers/BankConrtoller.cs
+++ TuanBuy/Controllers/BankConrtoller.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using TuanBuy.Models.Bank;
 using TuanBuy.Models.Bank.Utlity;
+using TuanBuy.Models.Entities;
 
 namespace TuanBuy.Controllers
 {
     public class BankController : Controller
     {
+        private readonly TuanBuyContext _dbContext;
+        public BankController(TuanBuyContext context)
+        {
+            _dbContext = context;
+        }
         /// <summary>
         /// 金流基本資料(可再移到Web.config或資料庫設定)
         /// </summary>
@@ -19,9 +28,9 @@ namespace TuanBuy.Controllers
             MerchantID = "MS134347960",
             HashKey = "0nnTkr1gNjKdSbbhBBedcyaBnaVm4eI5",
             HashIV = "C3eAKK9XZ4hxa4NP",
-            ReturnURL = "http://yourWebsitUrl/Bank/SpgatewayReturn",
-            NotifyURL = "http://yourWebsitUrl/Bank/SpgatewayNotify",
-            CustomerURL = "http://yourWebsitUrl/Bank/SpgatewayCustomer",
+            ReturnURL = "",
+            NotifyURL = "https://a4b7-1-164-244-135.ngrok.io/Bank/SpgatewayReturn",
+            CustomerURL = "https://a4b7-1-164-244-135.ngrok.io/MemberCenter/MyBuyProduct",
             AuthUrl = "https://ccore.spgateway.com/MPG/mpg_gateway",
             CloseUrl = "https://core.newebpay.com/API/CreditCard/Close"
         };
@@ -86,13 +95,13 @@ namespace TuanBuy.Controllers
                 // 商店備註
                 OrderComment = null,
                 // 信用卡 一次付清啟用(1=啟用、0或者未有此參數=不啟用)
-                CREDIT = 1,
+                CREDIT = null,
                 // WEBATM啟用(1=啟用、0或者未有此參數，即代表不開啟)
                 WEBATM = null,
                 // ATM 轉帳啟用(1=啟用、0或者未有此參數，即代表不開啟)
-                VACC = 1,
+                VACC = 0,
                 // 超商代碼繳費啟用(1=啟用、0或者未有此參數，即代表不開啟)(當該筆訂單金額小於 30 元或超過 2 萬元時，即使此參數設定為啟用，MPG 付款頁面仍不會顯示此支付方式選項。)
-                CVS = 1,
+                CVS = 0,
                 // 超商條碼繳費啟用(1=啟用、0或者未有此參數，即代表不開啟)(當該筆訂單金額小於 20 元或超過 4 萬元時，即使此參數設定為啟用，MPG 付款頁面仍不會顯示此支付方式選項。)
                 BARCODE = null,
                 LINEPAY = null
@@ -126,7 +135,7 @@ namespace TuanBuy.Controllers
             List<KeyValuePair<string, string>> tradeData = LambdaUtil.ModelToKeyValuePairList<TradeInfo>(tradeInfo);
             // 將List<KeyValuePair<string, string>> 轉換為 key1=Value1&key2=Value2&key3=Value3...
             var tradeQueryPara = string.Join("&", tradeData.Select(x => $"{x.Key}={x.Value}"));
-            tradeQueryPara = tradeQueryPara;
+            //tradeQueryPara = tradeQueryPara + "&SAMSUNGPAY=1&ANDROIDPAY=1";
             // AES 加密
             inputModel.TradeInfo = CryptoUtil.EncryptAESHex(tradeQueryPara, _bankInfoModel.HashKey, _bankInfoModel.HashIV);
             // SHA256 加密
@@ -156,6 +165,67 @@ namespace TuanBuy.Controllers
         }
 
 
+
+        /// <summary>
+        /// [智付通]
+        ///金流介接(結果: 支付完成 返回商店網址)
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult SpgatewayReturn()
+        {
+            //Request.LogFormData("SpgatewayReturn(支付完成)");
+            Console.WriteLine("進入該方式");
+            // Status 回傳狀態 
+            // MerchantID 回傳訊息
+            // TradeInfo 交易資料AES 加密
+            // TradeSha 交易資料SHA256 加密
+            // Version 串接程式版本
+            var collection = Request.Form;
+            if (string.Equals(collection["MerchantID"], _bankInfoModel.MerchantID) 
+                && string.Equals(collection["TradeSha"], CryptoUtil.EncryptSHA256($"HashKey={_bankInfoModel.HashKey}&{collection["TradeInfo"]}&HashIV={_bankInfoModel.HashIV}")))
+            {
+                var decryptTradeInfo = CryptoUtil.DecryptAESHex(collection["TradeInfo"], _bankInfoModel.HashKey, _bankInfoModel.HashIV);
+
+                // 取得回傳參數(ex:key1=value1&key2=value2),儲存為NameValueCollection
+                NameValueCollection decryptTradeCollection = HttpUtility.ParseQueryString(decryptTradeInfo);
+                SpgatewayOutputDataModel convertModel = LambdaUtil.DictionaryToObject<SpgatewayOutputDataModel>(decryptTradeCollection.AllKeys.ToDictionary(k => k, k => decryptTradeCollection[k]));
+                if(convertModel.Status == "SUCCESS")
+                {
+                    using(_dbContext)
+                    {
+                      var order = _dbContext.Order.Single(x => x.Id == int.Parse(convertModel.MerchantOrderNo));
+                        order.StateId = 3;
+                        _dbContext.SaveChanges();
+                    }
+                }
+                else
+                {
+                    using (_dbContext)
+                    {
+                        var order = _dbContext.Order.Single(x => x.Id == int.Parse(convertModel.MerchantOrderNo));
+                        order.StateId = 6;
+                        _dbContext.SaveChanges();
+                    }
+                }
+
+                Console.WriteLine(JsonConvert.SerializeObject(convertModel));
+
+                // TODO 將回傳訊息寫入資料庫
+
+                return Content(JsonConvert.SerializeObject(convertModel));
+            }
+            else
+            {
+                Console.WriteLine("MerchantID/TradeSha驗證錯誤");
+            }
+
+            return Content(string.Empty);
+        }
+        public ActionResult SpgatewayNotify()
+        {
+            return Content("hello");
+        }
 
         /// <summary>
         /// 銀行API測試
