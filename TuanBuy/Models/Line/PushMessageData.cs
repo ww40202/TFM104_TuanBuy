554176993ac4fbb7402fd624d79ddc368ddf7e67
Lynn <lynnmaxwell0706@gmail.com>
2022-04-13 15:09:34 +0800

    把LineBot相關搬運進專案

---
+++ TuanBuy/Models/Line/PushMessageData.cs
@@ -0,0 +1,14 @@
+﻿namespace TuanBuy.Models.Line
+{
+    public class PushMessageData
+    {
+        public string to { get; set; }
+        public PushMessage[] messages { get; set; }
+    }
+
+    public class PushMessage
+    {
+        public string type { get; set; }
+        public string text { get; set; }
+    }
+}
