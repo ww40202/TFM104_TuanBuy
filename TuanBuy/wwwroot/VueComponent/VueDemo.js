a356908c39d1a99faabfdc6ff9ef013d8fe0f17b
Lynn <lynnmaxwell0706@gmail.com>
2022-04-13 21:14:08 +0800

    新增訂單管理頁

--- TuanBuy/wwwroot/VueComponent/VueDemo.js
+++ TuanBuy/wwwroot/VueComponent/VueDemo.js
@@ -97,7 +97,7 @@
                         <div id="collapseFive" class="collapse" aria-labelledby="headingFiVE"
                             data-parent="#accordionExample">
                             <div class="collapsecard-body bgcf5">
-                                <a href="#">賣家訊息</a>
+                                <a href="/MemberCenter/OrderManger">訂單管理</a>
                             </div>
                         </div>
 
